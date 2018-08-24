c19040324a256c36e8c34ce88ad03b1b57840a59
louisqvo <louisqvo@gmail.com>
2018-08-23 20:53:13 -0400

    post friend info and recieving match

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -1,41 +1,28 @@
 var friendList = require("../data/friend.js");
-// var waitListData = require("../data/waitinglistData");
 
 module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
   
     app.get("/api/friends", function(req, res) {
       res.json(friendList);
     });
 
-    // app.post("/app/friends", function(req, res){
-    //     friendList.push(req.body);
-    // })
+    app.post("/api/friends", function(req, res){
+        
+        var index = 0;
+        var total = 40;
+        for(i in friendList){
+            var diff = 0;
+            for(var j = 0; j < 10; j++){
+                diff += Math.abs( parseInt(friendList[i].scores[j]) - parseInt(req.body.scores[j]))
+            }
+
+            if(diff < total){
+                index = i;
+                total = diff;
+            }
+        }
+        friendList.push(req.body);
+        res.json(friendList[index])
+    })
   
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-  
-    // app.post("/api/tables", function(req, res) {
-    //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    //   // It will do this by sending out the value "true" have a table
-    //   // req.body is available since we're using the body-parser middleware
-    //   if (tableData.length < 5) {
-    //     tableData.push(req.body);
-    //     res.json(true);
-    //   }
-    //   else {
-    //     waitListData.push(req.body);
-    //     res.json(false);
-    //   }
-    // });
 };
-  
